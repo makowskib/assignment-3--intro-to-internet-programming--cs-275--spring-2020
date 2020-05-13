2fe02d5f30e8684a61f2d27b175fb0e4c51acdb1
makowskib <58569878+makowskib@users.noreply.github.com>
2020-05-13 07:50:29 -0400

    changed toggle to add/remove

--- js/app.js
+++ js/app.js
@@ -3,39 +3,39 @@ let letter =document.querySelectorAll('td');
 window.onload=()=>{
     for(let i=0; i < letter.length; i++){
         letter[i].addEventListener('click',()=>{
-            letter[i].classList.toggle('transform');
+            letter[i].classList.add('transform');
         });
     }
     //letterA
     letter[0].addEventListener('click', ()=>{
         if(letter[0].classList.contains('transform')){
-            letter[1].classList.toggle('transform');
-            letter[2].classList.toggle('transform');
-            letter[3].classList.toggle('transform');
+            letter[1].classList.remove('transform');
+            letter[2].classList.remove('transform');
+            letter[3].classList.remove('transform');
         }
     });
     //letterB
     letter[1].addEventListener('click', ()=>{
         if(letter[1].classList.contains('transform')){
-            letter[0].classList.toggle('transform');
-            letter[2].classList.toggle('transform');
-            letter[3].classList.toggle('transform');
+            letter[0].classList.remove('transform');
+            letter[2].classList.remove('transform');
+            letter[3].classList.remove('transform');
         }
     });
     //letterC
     letter[2].addEventListener('click', ()=>{
         if(letter[2].classList.contains('transform')){
-            letter[1].classList.toggle('transform');
-            letter[0].classList.toggle('transform');
-            letter[3].classList.toggle('transform');
+            letter[1].classList.remove('transform');
+            letter[0].classList.remove('transform');
+            letter[3].classList.remove('transform');
         }
     });
     //letterD
     letter[3].addEventListener('click', ()=>{
         if(letter[3].classList.contains('transform')){
-            letter[1].classList.toggle('transform');
-            letter[2].classList.toggle('transform');
-            letter[0].classList.toggle('transform');
+            letter[1].classList.remove('transform');
+            letter[2].classList.remove('transform');
+            letter[0].classList.remove('transform');
         }
     });
 }
