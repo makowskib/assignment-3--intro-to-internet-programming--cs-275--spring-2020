ddcf7772f9a4647b6c982872f96488bf7e9a9eba
makowskib <58569878+makowskib@users.noreply.github.com>
2020-05-13 07:48:57 -0400

    added toggle and other letters

--- js/app.js
+++ js/app.js
@@ -6,14 +6,38 @@ window.onload=()=>{
             letter[i].classList.toggle('transform');
         });
     }
+    //letterA
     letter[0].addEventListener('click', ()=>{
         if(letter[0].classList.contains('transform')){
-            letter[1].classList.remove('transform');
-            letter[2].classList.remove('transform');
-            letter[3].classList.remove('transform');
+            letter[1].classList.toggle('transform');
+            letter[2].classList.toggle('transform');
+            letter[3].classList.toggle('transform');
         }
     });
-
+    //letterB
+    letter[1].addEventListener('click', ()=>{
+        if(letter[1].classList.contains('transform')){
+            letter[0].classList.toggle('transform');
+            letter[2].classList.toggle('transform');
+            letter[3].classList.toggle('transform');
+        }
+    });
+    //letterC
+    letter[2].addEventListener('click', ()=>{
+        if(letter[2].classList.contains('transform')){
+            letter[1].classList.toggle('transform');
+            letter[0].classList.toggle('transform');
+            letter[3].classList.toggle('transform');
+        }
+    });
+    //letterD
+    letter[3].addEventListener('click', ()=>{
+        if(letter[3].classList.contains('transform')){
+            letter[1].classList.toggle('transform');
+            letter[2].classList.toggle('transform');
+            letter[0].classList.toggle('transform');
+        }
+    });
 }
 
 
